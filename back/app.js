772054946561f4be7749fe7d4b5dfe52c19e0812
Alexis Ferrandis <alexis.1.ferrandis@gmail.com>
2021-11-12 15:30:42 +0100

    Routes and controllers creation

--- back/app.js
+++ back/app.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const Sauce = require("./models/sauce");
-
-const app = express();
+const sauceRoutes = require("./routes/sauce.routes");
 
 mongoose
 	.connect("mongodb+srv://alexis:hottakesPWD@cluster0.5iykr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
 	.then(() => console.log("Connected to MongoDB"))
 	.catch(() => console.log("Cannot connect to MongoDB"));
+
+const app = express();
 
 app.use((req, res, next) => {
 	res.setHeader("Access-Control-Allow-Origin", "*");
@@ -18,37 +18,6 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 
-app.post("/api/sauces", (req, res, next) => {
-	delete req.body._id;
-	const sauce = new Sauce({
-		...req.body,
-	});
-	sauce
-		.save()
-		.then(() => res.status(201).json({ message: "Sauce saved" }))
-		.catch((error) => res.status(400).json({ error }));
-});
-
-app.use("/api/auth/login", (req, res, next) => {
-	const stuff = [
-		{
-			_id: "oeihfzeoi",
-			title: "Mon premier objet",
-			description: "Les infos de mon premier objet",
-			imageUrl: "https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
-			price: 4900,
-			userId: "qsomihvqios",
-		},
-		{
-			_id: "oeihfzeomoihi",
-			title: "Mon deuxième objet",
-			description: "Les infos de mon deuxième objet",
-			imageUrl: "https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
-			price: 2900,
-			userId: "qsomihvqios",
-		},
-	];
-	res.status(200).json(stuff);
-});
+app.use("/api/stuff", sauceRoutes);
 
 module.exports = app;
