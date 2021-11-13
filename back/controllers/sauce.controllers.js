c9493c47947577a6a2578d653882957cd646d401
Alexis Ferrandis <alexis.1.ferrandis@gmail.com>
2021-11-13 16:27:08 +0100

    Add, modify & delete a sauce enable

--- back/controllers/sauce.controllers.js
+++ back/controllers/sauce.controllers.js
@@ -1,9 +1,11 @@
 const Sauce = require("../models/Sauce");
 
 exports.createSauce = (req, res, next) => {
-	delete req.body._id;
+	const sauceObject = JSON.parse(req.body.sauce);
+	delete sauceObject._id;
 	const sauce = new Sauce({
-		...req.body,
+		...sauceObject,
+		imageUrl: `${req.protocol}://${req.get("host")}/images/${req.file.filename}`,
 	});
 	sauce
 		.save()
@@ -12,7 +14,13 @@ exports.createSauce = (req, res, next) => {
 };
 
 exports.modifySauce = (req, res, next) => {
-	Sauce.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+	const sauceObject = req.file
+		? {
+				...JSON.parse(req.body.sauce),
+				imageUrl: `${req.protocol}://${req.get("host")}/images/${req.file.filename}`,
+		  }
+		: { ...req.body };
+	Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
 		.then(() => res.status(200).json({ message: "Sauce has been modified" }))
 		.catch((error) => res.status(400).json({ error }));
 };
--- back/routes/sauce.routes.js
+++ back/routes/sauce.routes.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
+
 const sauceCtrl = require("../controllers/sauce.controllers");
 const auth = require("../middleware/auth");
+const multer = require("../middleware/multer-config");
 
-router.post("/", auth, sauceCtrl.createSauce);
-router.put("/:id", auth, sauceCtrl.modifySauce);
-router.delete("/:id", auth, sauceCtrl.deleteSauce);
-router.get("/:id", auth, sauceCtrl.getOneSauce);
 router.get("/", auth, sauceCtrl.getAllSauces);
+router.post("/", auth, multer, sauceCtrl.createSauce);
+router.get("/:id", auth, sauceCtrl.getOneSauce);
+router.put("/:id", auth, multer, sauceCtrl.modifySauce);
+router.delete("/:id", auth, sauceCtrl.deleteSauce);
 
 module.exports = router;
